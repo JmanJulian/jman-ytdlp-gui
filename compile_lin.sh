6dfae53868a051c055b149b60478f8270899e015
JmanJulian <107344511+JmanJulian@users.noreply.github.com>
2022-06-13 09:45:52 +0300

    Add files via upload

---
+++ compile_lin.sh
@@ -0,0 +1,1 @@
+pyinstaller --icon=img/Youtube-dl.ico --onefile --add-data="img/video.png;img" --add-data="img/audio.png;img" --add-data="img/Youtube-dl.ico;img" ytdlp-gui-en.py
---
+++ compile_win.cmd
@@ -0,0 +1,2 @@
+@echo off
+pyinstaller.exe --icon=img/Youtube-dl.ico --onefile --add-data="img/video.png;img" --add-data="img/audio.png;img" --add-data="img/Youtube-dl.ico;img" ytdlp-gui-en.py
