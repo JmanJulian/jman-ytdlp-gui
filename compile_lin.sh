6062bc283f7516f7b273b99a42d58654a9d43802
JmanJulian <107344511+JmanJulian@users.noreply.github.com>
2022-07-03 11:24:51 +0300

    Update compile_lin.sh

--- compile_lin.sh
+++ compile_lin.sh
@@ -1,1 +1,2 @@
+#for this to work you must have pyinstaller module installed -> "pip install pyinstaller" or "pip3 install pyinstaller"
 pyinstaller --icon=img/Youtube-dl.ico --add-data="img/Youtube-dl.ico:img" --onefile ytdlp-gui.pyw
