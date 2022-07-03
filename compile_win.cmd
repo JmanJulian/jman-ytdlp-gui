fa7406f6048953560d69b69411e2eadc50c3277a
JmanJulian <107344511+JmanJulian@users.noreply.github.com>
2022-07-03 10:12:06 +0300

    Update compile_win.cmd
    remove include images because they are part of script now

--- compile_win.cmd
+++ compile_win.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM for this to work you must have pyinstaller package. INSTALL COMAND: "pip install pyinstaller"
-pyinstaller.exe --icon=img/Youtube-dl.ico --onefile --add-data="img/video.png;img" --add-data="img/audio.png;img" --add-data="img/Youtube-dl.ico;img" ytdlp-gui.pyw
+pyinstaller.exe --icon=img/Youtube-dl.ico --onefile --noconsole ytdlp-gui.pyw
