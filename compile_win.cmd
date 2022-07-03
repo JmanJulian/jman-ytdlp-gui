f48b216c6c332ab7c05470b6b7bfbdd37955fb58
JmanJulian <107344511+JmanJulian@users.noreply.github.com>
2022-07-03 10:51:28 +0300

    Delete compile_win.cmd

--- compile_win.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-REM for this to work you must have pyinstaller package. INSTALL COMAND: "pip install pyinstaller"
-pyinstaller.exe --icon=img/Youtube-dl.ico --onefile --noconsole ytdlp-gui.pyw
