40a2a8314038acf3617c6ca858e36f1e314db9fa
JmanJulian <107344511+JmanJulian@users.noreply.github.com>
2022-07-03 11:27:58 +0300

    Update compile_win.cmd

--- compile_win.cmd
+++ compile_win.cmd
@@ -1,3 +1,3 @@
 @echo off
-REM for this to work you must have pyinstaller module installed to get it run in command prompt  -> "pip install pyinstaller"
+REM for this to work you must have pyinstaller module installed to get it run in command prompt: "pip install pyinstaller"
 pyinstaller.exe --icon=img/Youtube-dl.ico --add-data="img/Youtube-dl.ico;img" --noconsole --onefile ytdlp-gui.pyw
